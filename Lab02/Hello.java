1ee851c7ae0d3a32fdf0024fedde73a7f8ff0857
JuicePouch <loloymann@gmail.com>
2021-09-21 18:25:41 -0700

    Hello Class with student info

---
+++ Lab02/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		System.out.println("antonioabaya@csus.edu - Antonio Abaya.");
+		System.out.println("antonioabaya@csus.edu - Antonio Abaya.");
+		System.out.println("antonioabaya@csus.edu - Antonio Abaya.");
+		System.out.println("antonioabaya@csus.edu - Antonio Abaya.");
+		System.out.println("antonioabaya@csus.edu - Antonio Abaya.");
+		System.out.println("antonioabaya@csus.edu - Antonio Abaya.");
+}
+
+}
